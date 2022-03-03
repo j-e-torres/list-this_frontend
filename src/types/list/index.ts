6fbda90e14ac9262d829a928345f3e5fe8608eb6
juan torres <juan.torres@halopowered.com>
2022-03-02 20:19:07 -0500

    add task to types

--- src/types/list/index.ts
+++ src/types/list/index.ts
@@ -1,8 +1,10 @@
-import { User } from '../user/';
+import { User } from '../user';
+import { Task } from '../task';
 
 export interface List {
   id: string;
   listOwner: User,
   listName: string;
   listNotes: string;
+  tasks: Task[];
 }
